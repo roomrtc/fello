4cb63807b83e45f2846790315676820474d90093
Vunb <nhubaovu@gmail.com>
2015-02-01 03:18:22 +0700

    config + intergate easyrtc

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,22 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
+
 module.exports.bootstrap = function(cb) {
+  var easyrtc = require("easyrtc");
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+
+  //easyrtc.listen(app, sails.hooks.http.server);
+  // http://stackoverflow.com/a/24271007
+  var easyrtcOptions = {
+    //apiPublicFolder: 'js',
+    demosEnable: false
+  };
+
+  //easyrtc.setOption('apiPublicFolder', 'js');
+  easyrtc.listen(sails.hooks.http.app, sails.io, easyrtcOptions);
   cb();
 };
